a261a2c059589e9dbf30e38c79924bac8c389506
ttbachyinsda <gfz14@mails.tsinghua.edu.cn>
2017-04-27 20:21:24 +0800

    whfiowheofhewofhi

--- a.sh
+++ a.sh
@@ -1,58 +1,116 @@
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/400.perlbench.out.trace.gz -o 400.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 400.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/400.perlbench.out.trace.gz -o 400.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 400.1.txt
+echo 400.perlbench.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/400.perlbench.out.trace.gz -o 400.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 400.2.txt
 echo 400.perlbench.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/401.bzip2.out.trace.gz -o 401.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 401.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/401.bzip2.out.trace.gz -o 401.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 401.1.txt
+echo 401.bzip2.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/401.bzip2.out.trace.gz -o 401.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 401.2.txt
 echo 401.bzip2.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/403.gcc.out.trace.gz -o 403.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 403.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/403.gcc.out.trace.gz -o 403.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 403.1.txt
+echo 403.gcc.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/403.gcc.out.trace.gz -o 403.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 403.2.txt
 echo 403.gcc.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/410.bwaves.out.trace.gz -o 410.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 410.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/410.bwaves.out.trace.gz -o 410.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 410.1.txt
+echo 410.bwaves.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/410.bwaves.out.trace.gz -o 410.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 410.2.txt
 echo 410.bwaves.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/416.gamess.out.trace.gz -o 416.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 416.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/416.gamess.out.trace.gz -o 416.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 416.1.txt
+echo 416.gamess.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/416.gamess.out.trace.gz -o 416.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 416.2.txt
 echo 416.gamess.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/429.mcf.out.trace.gz -o 429.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 429.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/429.mcf.out.trace.gz -o 429.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 429.1.txt
+echo 429.mcf.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/429.mcf.out.trace.gz -o 429.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 429.2.txt
 echo 429.mcf.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/433.milc.out.trace.gz -o 433.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 433.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/433.milc.out.trace.gz -o 433.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 433.1.txt
+echo 433.milc.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/433.milc.out.trace.gz -o 433.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 433.2.txt
 echo 433.milc.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/434.zeusmp.out.trace.gz -o 434.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 434.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/434.zeusmp.out.trace.gz -o 434.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 434.1.txt
+echo 434.zeusmp.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/434.zeusmp.out.trace.gz -o 434.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 434.2.txt
 echo 434.zeusmp.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/435.gromacs.out.trace.gz -o 435.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 435.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/435.gromacs.out.trace.gz -o 435.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 435.1.txt
+echo 435.gromacs.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/435.gromacs.out.trace.gz -o 435.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 435.2.txt
 echo 435.gromacs.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/436.cactusADM.out.trace.gz -o 436.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 436.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/436.cactusADM.out.trace.gz -o 436.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 436.1.txt
+echo 436.cactusADM.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/436.cactusADM.out.trace.gz -o 436.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 436.2.txt
 echo 436.cactusADM.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/437.leslie3d.out.trace.gz -o 437.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 437.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/437.leslie3d.out.trace.gz -o 437.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 437.1.txt
+echo 437.leslie3d.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/437.leslie3d.out.trace.gz -o 437.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 437.2.txt
 echo 437.leslie3d.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/444.namd.out.trace.gz -o 444.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 444.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/444.namd.out.trace.gz -o 444.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 444.1.txt
+echo 444.namd.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/444.namd.out.trace.gz -o 444.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 444.2.txt
 echo 444.namd.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/445.gobmk.out.trace.gz -o 445.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 445.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/445.gobmk.out.trace.gz -o 445.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 445.1.txt
+echo 445.gobmk.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/445.gobmk.out.trace.gz -o 445.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 445.2.txt
 echo 445.gobmk.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/447.dealII.out.trace.gz -o 447.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 447.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/447.dealII.out.trace.gz -o 447.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 447.1.txt
+echo 447.dealII.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/447.dealII.out.trace.gz -o 447.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 447.2.txt
 echo 447.dealII.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/450.soplex.out.trace.gz -o 450.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 450.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/450.soplex.out.trace.gz -o 450.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 450.1.txt
+echo 450.soplex.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/450.soplex.out.trace.gz -o 450.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 450.2.txt
 echo 450.soplex.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/453.povray.out.trace.gz -o 453.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 453.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/453.povray.out.trace.gz -o 453.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 453.1.txt
+echo 453.povray.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/453.povray.out.trace.gz -o 453.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 453.2.txt
 echo 453.povray.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/454.calculix.out.trace.gz -o 454.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 454.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/454.calculix.out.trace.gz -o 454.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 454.1.txt
+echo 454.calculix.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/454.calculix.out.trace.gz -o 454.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 454.2.txt
 echo 454.calculix.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/456.hmmer.out.trace.gz -o 456.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 456.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/456.hmmer.out.trace.gz -o 456.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 456.1.txt
+echo 456.hmmer.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/456.hmmer.out.trace.gz -o 456.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 456.2.txt
 echo 456.hmmer.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/458.sjeng.out.trace.gz -o 458.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 458.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/458.sjeng.out.trace.gz -o 458.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 458.1.txt
+echo 458.sjeng.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/458.sjeng.out.trace.gz -o 458.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 458.2.txt
 echo 458.sjeng.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/459.GemsFDTD.out.trace.gz -o 459.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 459.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/459.GemsFDTD.out.trace.gz -o 459.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 459.1.txt
+echo 459.GemsFDTD.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/459.GemsFDTD.out.trace.gz -o 459.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 459.2.txt
 echo 459.GemsFDTD.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/462.libquantum.out.trace.gz -o 462.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 462.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/462.libquantum.out.trace.gz -o 462.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 462.1.txt
+echo 462.libquantum.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/462.libquantum.out.trace.gz -o 462.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 462.2.txt
 echo 462.libquantum.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/464.h264ref.out.trace.gz -o 464.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 464.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/464.h264ref.out.trace.gz -o 464.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 464.1.txt
+echo 464.h264ref.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/464.h264ref.out.trace.gz -o 464.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 464.2.txt
 echo 464.h264ref.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/465.tonto.out.trace.gz -o 465.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 465.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/465.tonto.out.trace.gz -o 465.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 465.1.txt
+echo 465.tonto.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/465.tonto.out.trace.gz -o 465.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 465.2.txt
 echo 465.tonto.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/470.lbm.out.trace.gz -o 470.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 470.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/470.lbm.out.trace.gz -o 470.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 470.1.txt
+echo 470.lbm.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/470.lbm.out.trace.gz -o 470.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 470.2.txt
 echo 470.lbm.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/471.omnetpp.out.trace.gz -o 471.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 471.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/471.omnetpp.out.trace.gz -o 471.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 471.1.txt
+echo 471.omnetpp.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/471.omnetpp.out.trace.gz -o 471.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 471.2.txt
 echo 471.omnetpp.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/473.astar.out.trace.gz -o 473.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 473.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/473.astar.out.trace.gz -o 473.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 473.1.txt
+echo 473.astar.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/473.astar.out.trace.gz -o 473.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 473.2.txt
 echo 473.astar.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/482.sphinx3.out.trace.gz -o 482.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 482.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/482.sphinx3.out.trace.gz -o 482.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 482.1.txt
+echo 482.sphinx3.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/482.sphinx3.out.trace.gz -o 482.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 482.2.txt
 echo 482.sphinx3.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/483.xalancbmk.out.trace.gz -o 483.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 483.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/483.xalancbmk.out.trace.gz -o 483.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 483.1.txt
+echo 483.xalancbmk.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/483.xalancbmk.out.trace.gz -o 483.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 483.2.txt
 echo 483.xalancbmk.out.trace.gz 2
-(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/999.specrand.out.trace.gz -o 999.2 -cache UL3:1024:64:16 -LLCrepl 0) >& 999.2.txt
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/999.specrand.out.trace.gz -o 999.1 -cache UL3:1024:64:16 -LLCrepl 1) >& 999.1.txt
+echo 999.specrand.out.trace.gz 1
+(time ../bin/CMPsim.usetrace.64 -threads 1 -t ../traces/999.specrand.out.trace.gz -o 999.2 -cache UL3:1024:64:16 -LLCrepl 2) >& 999.2.txt
 echo 999.specrand.out.trace.gz 2
